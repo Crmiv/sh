491dcc3bdb7d98166298d29643dc21df3af151cc
Crmiv <liujunning.magic@gmail.com>
2014-06-13 01:16:43 +0800

    suckcode

--- sed/a.sh
+++ sed/a.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 echo hello world | sed s/hello/Hello/
+#-n regex expression only affect the first line
+sed -n 's/^/<text>/;s/$/<\/text>/p;q' text
+
+#you can use a sed file
+#like that
+#a.sed
+#
+
+#!/usr/bin/sed
+
+s/^/<h1>/
+s/$/</h1>/
+q
+
+#use sed -f a.sed text
+
+# -C count number
+grep -E '\<(THE|the|The)\>' text
+sed '1 i\some ' text2 
+
 
