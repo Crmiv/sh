dfca82de89f853335a31064fd5cf84c13b66a0f2
Crmiv <liujunning.magic@gmail.com>
2014-06-14 00:10:29 +0800

    sed

--- sed/sedCommand.sh
+++ sed/sedCommand.sh
@@ -9,4 +9,13 @@ sed 's/pattern/replacement/g' file
 
 #移除空白行
 sed '/^$/d' file
-#
+
+#replace 三位数
+sed -i 's/\b[0-9]\{3\}\b/NUMBER/g' filename
+
+#cite the former that match 
+echo this is an example | sed 's/\w\+/[&]/g'
+
+#back reference
+echo seven EIGHT | sed 's/\([a-z]\+\) \([a-z]\+\)/\2 \1/'
+
